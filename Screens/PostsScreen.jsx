ec6ee7d6486f3073d6d61fd596886bc7c6b329ff
AlinaS13 <soulofangel13@gmail.com>
2023-06-25 21:47:22 +0300

    add hw5

--- Screens/PostsScreen.jsx
+++ Screens/PostsScreen.jsx
@@ -1,13 +1,28 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
+import { UserInfo } from "./UserInfo";
 
 export const PostsScreen = () => {
+  const navigation = useNavigation();
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Публікації</Text>
+    <View style={styles.container}>
+      <UserInfo />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // justifyContent: "space-between",
+    // alignItems: "center",
+    paddingRight: 16,
+    paddingLeft: 22,
+    paddingTop: 32,
+    paddingBottom: 22,
+    backgroundColor: "#ffffff",
+  },
+});
